703b79060d2290c8ee2db382025f0a3fc38d390f
Yaroslav Shakalov <yaroslavshakalov888@gmail.com>
2021-11-05 16:05:10 +0200

    back end fixes comleted

--- serverapp/index.js
+++ serverapp/index.js
@@ -10,6 +10,15 @@ const app = express();
 app.use(express.json());
 
 app.use('/api', router);
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 const start = async () => {
   try {
