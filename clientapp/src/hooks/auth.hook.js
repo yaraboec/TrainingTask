dabef178674bca9b2114add99336853fc0f4334e
yaroslav.shakalov <yaroslavshakalov888@gmail.com>
2021-11-10 09:07:40 +0200

    front token

--- clientapp/src/hooks/auth.hook.js
+++ clientapp/src/hooks/auth.hook.js
@@ -1,12 +1,16 @@
 import { useState, useEffect, useCallback } from 'react';
+import jwtDecode from 'jwt-decode';
 
 const useAuth = () => {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState('');
   const [isAlready, setIsAlready] = useState(false);
 
   const login = useCallback((jwt) => {
     setToken(jwt);
     localStorage.setItem('token', JSON.stringify(jwt));
+    console.log(jwt);
+    const decoded = jwtDecode(jwt);
+    console.log(decoded.id);
   }, []);
 
   const logout = () => {
